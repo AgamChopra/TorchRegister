6c703a4770d62dc691d13f88bbc2d64fd2d94d73
Agam Chopra <work.agamchopra@gmail.com>
2023-04-17 05:33:15 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email='achopra4@uw.edu'
     description='Common medical 3D image registration methods such as rigid, affine, and flow field for PyTorch.'
     packages=find_packages(),    
-    install_requires=['torch', 'numpy', 'tqdm', 'matplotlib'],
+    install_requires=['torch >= 2.0.0', 'numpy >= 1.24.1', 'tqdm >= 4.65.0', 'matplotlib  >= 3.7.1'],
 )

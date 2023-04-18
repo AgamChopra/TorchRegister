f514db573e0e3a57e62464dc242a97cfcf121a40
Agam main <work.agamchopra@gmail.com>
2023-04-17 18:20:33 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TorchRegister',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/AgamChopra/TorchRegister',
     author='Agam Chopra',
     author_email='achopra4@uw.edu',
